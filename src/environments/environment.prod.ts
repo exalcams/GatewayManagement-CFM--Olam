f365041336b68ed5689fad042c322c6a4b0e734b
Pramod D P <Pramod D P@EIA129.exalca.corp>
2019-12-23 11:29:06 +0530

    envirnonment changes 23th dec 2019

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    baseAddress : 'http://172.29.0.99:8868/v1/', //Development Server
-    //baseAddress : 'http://sgtcx-truckmgmt.olamdomain.com:8868/v1/', //Production Server(Singapore)
+    //baseAddress : 'http://172.29.0.99:8868/v1/', //Development Server
+    baseAddress : 'http://sgtcx-truckmgmt.olamdomain.com:8868/v1/', //Production Server(Singapore)
     clientId : 'ngAuthApp'
 };
